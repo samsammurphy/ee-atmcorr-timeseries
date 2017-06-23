625393b1229deba577458a1c913867f15f488d9a
Sam Murphy <samsammurphy@gmail.com>
2017-06-22 18:08:54 -1000

    test module added (ideas)

---
+++ atmcorr/tests.py
@@ -0,0 +1,14 @@
+"""
+tests.py, Sam Murphy (2017-06-22)
+
+Bunch of tests for earth engine atmospherically corrected time series
+"""
+
+# run quick checks
+def input_check(satellite_missions, geom, startDate, stopDate):
+    
+    # satellite mission check
+    # dates check
+
+
+    return 'PASSED'
