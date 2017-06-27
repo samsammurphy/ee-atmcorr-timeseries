11f836eaeb79279756fccf246d4020ab94cd8f84
Sam Murphy <samsammurphy@gmail.com>
2017-06-26 18:03:01 -1000

    renamed tests.py to quick_check.py

--- atmcorr/quick_checks.py.py
+++ atmcorr/quick_checks.py.py
@@ -1,7 +1,8 @@
 """
-tests.py, Sam Murphy (2017-06-22)
+quick_checks.py, Sam Murphy (2017-06-22)
 
-Bunch of tests for earth engine atmospherically corrected time series
+A few quick checks to test if user input is reasonable
+(i.e. less likely to hit bugs in my code)
 """
 
 # run quick checks
