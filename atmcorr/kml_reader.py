4cd9b6c7acd0b1109e4e3e9733513eecba1a7f40
Sam Murphy <samsammurphy@gmail.com>
2017-07-18 14:22:33 -1000

    kml_reader

---
+++ atmcorr/kml_reader.py
@@ -0,0 +1,42 @@
+"""
+Reads kml files
+"""
+
+import os
+import ee
+from fastkml import kml
+
+def read_kml(fileName, polygonName):
+  """
+  Atmospherically corrects radiance using correction coefficients
+  at perihelion adjusted for Earth's ellipitcal orbit
+  """
+ 
+  # read kml from file 
+  base_dir = os.path.dirname(os.getcwd())
+  fpath = os.path.join(base_dir,'files','kml',fileName)
+  with open(fpath,'rb') as file:
+    kml_string = file.read()
+
+  # kml object
+  k = kml.KML()
+  k.from_string(kml_string)
+  
+  # parse the object
+  document = list(k.features())
+  folder = list(document[0].features())
+  polygons = list(folder[0].features())
+  names = [p.name for p in polygons]
+
+  # find a polygon
+  index = names.index(polygonName)
+  polygon = polygons[index].geometry
+
+  # get coordinates
+  x, y = polygon.exterior.coords.xy
+  coords = [xy for xy in list(zip(x,y))]
+
+  # create earth engine geometry
+  geom = ee.Geometry.Polygon(coords)
+
+  return geom
