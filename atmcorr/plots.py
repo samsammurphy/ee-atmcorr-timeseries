644ec7cccbcb2775e21ee51c91acf1af4af8abf9
Sam Murphy <samsammurphy@gmail.com>
2017-08-04 13:20:39 -1000

    totally new plots (hue stretch, value, saturation

--- atmcorr/plots.py
+++ atmcorr/plots.py
@@ -1,152 +1,56 @@
-"""
-plots.py
-"""
-
+import pandas as pd
 from matplotlib import pylab as plt
-import datetime
+import matplotlib.dates as mdates
 
-def visPlot(timeSeries):
-  """
-  plots visible wavebands
-  """
-
-  vizBandSwitch = {
-    'Sentinel2':('B2','B3','B4'),
-    'Landsat8':('B2','B3','B4'),
-    'Landsat7':('B1','B2','B3'),
-    'Landsat5':('B1','B2','B3'),
-    'Landsat4':('B1','B2','B3')
-
-  }
-
-  vizBands = vizBandSwitch[timeSeries['mission']]
-
-  plt.plot(timeSeries['dates'], timeSeries[vizBands[0]], 'blue')
-  plt.plot(timeSeries['dates'], timeSeries[vizBands[1]], 'green')
-  plt.plot(timeSeries['dates'], timeSeries[vizBands[2]], 'red')
-  plt.title('RGB')
-  plt.ylim([0,1])
-  plt.show()
-
-def nirPlot(timeSeries):
-
-  nirSwitch = {
-    'Sentinel2':{'bands':['B5','B6','B7','B7'],
-                 'colors':['#cbc9e2', '#9e9ac8', '#756bb1', '#54278f'],
-                 'labels':['705 nm', '740 nm', '783 nm', '842 nm']},
-
-    'Landsat8':{'bands':['B5'],
-                 'colors':['#cbc9e2'],
-                 'labels':['865 nm']},
-
-    'Landsat7':{'bands':['B4'],
-                 'colors':['#cbc9e2'],
-                 'labels':['830 nm']},
-
-    'Landsat5':{'bands':['B4'],
-                 'colors':['#cbc9e2'],
-                 'labels':['830 nm']},
-
-    'Landsat4':{'bands':['B4'],
-                 'colors':['#cbc9e2'],
-                 'labels':['830 nm']}
-
-  }
-
-  config = nirSwitch[timeSeries['mission']]
-
-  for i, band in enumerate(config['bands']):
-    plt.plot(timeSeries['dates'], timeSeries[band], config['colors'][i], label=config['labels'][i])
+def figure_plotting_space():
+    """
+    defines the plotting space
+    """
   
-  plt.title('Near Infrared')
-  plt.ylim([0,1])
-  plt.legend()
-  plt.show()
+    fig = plt.figure(figsize=(10,10))
+    bar_height = 0.04
+    mini_gap = 0.03
+    gap = 0.05
+    graph_height = 0.24
 
-def swirPlot(timeSeries):
-
-  swirSwitch = {
-    'Sentinel2':{'bands':['B11','B12'],
-                 'colors':['#993404', '#fe9929'],
-                 'labels':['1610 nm', '2190 nm']},
-
-    'Landsat8':{'bands':['B6','B7'],
-                 'colors':['#993404', '#fe9929'],
-                 'labels':['1610 nm', '2200 nm']},
-
-    'Landsat7':{'bands':['B5','B7'],
-                 'colors':['#993404', '#fe9929'],
-                 'labels':['1650 nm', '2270 nm']},
-
-    'Landsat5':{'bands':['B5','B7'],
-                 'colors':['#993404', '#fe9929'],
-                 'labels':['1650 nm', '2270 nm']},
-
-    'Landsat4':{'bands':['B5','B7'],
-                 'colors':['#993404', '#fe9929'],
-                 'labels':['1650 nm', '2270 nm']}
-
-  }
-
-  config = swirSwitch[timeSeries['mission']]
-
-  for i, band in enumerate(config['bands']):
-    plt.plot(timeSeries['dates'], timeSeries[band], config['colors'][i], label=config['labels'][i])
-  
-  plt.title('Short-wave Infrared')
-  plt.ylim([0,1])
-  plt.legend()
-  plt.show()
-
-def atmPlot(timeSeries):
-
-  atmSwitch = {
-    'Sentinel2':{'bands':['B1','B9','B10'],
-                 'colors':['#c994c7', '#225ea8', '#c7e9b4'],
-                 'labels':['aerosol (443 nm)', 'water vapour (940 nm)', 'cirrus (1375 nm)']},
-
-    'Landsat8':{'bands':['B1','B9'],
-                 'colors':['#c994c7','#c7e9b4'],
-                 'labels':['aerosol (440 nm)', 'cirrus (1370 nm)']},
-
-    'Landsat7':None,
-
-    'Landsat5':None,
-
-    'Landsat4':None
-
-  }
-
-  config = atmSwitch[timeSeries['mission']]
-
-  if not config:
-    return None
-
-  for i, band in enumerate(config['bands']):
-    plt.plot(timeSeries['dates'], timeSeries[band], config['colors'][i], label=config['labels'][i])
+    axH = fig.add_axes([0.1,gap+3*graph_height+2.5*mini_gap,0.87,bar_height])
+    axS = fig.add_axes([0.1,gap+2*graph_height+2*mini_gap,0.87,graph_height])
+    axV = fig.add_axes([0.1,gap+graph_height+mini_gap,0.87,graph_height])
     
-  plt.title('Atmospheric')
-  plt.ylim([0,1])
-  plt.legend()
-  plt.show()
+    return fig, axH, axS, axV
 
-  return 1
+def plot_colorbar(ax,image,ylabel=False):
+    """
+    display a colorbar (e.g. hue-stretch)
+    """
+        
+    # plot image inside of figure 'axes'
+    ax.imshow(image, interpolation='nearest', aspect='auto')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_ylabel(ylabel)
 
-def plot_timeseries(timeSeries, plotType):
+def plot_timeseries(DF, ax, name, startDate, stopDate, ylim=False):
+    """
+    plots timeseries graphs
+    """
   
-  timeSeries['dates'] = [datetime.datetime.utcfromtimestamp(dt) for dt in timeSeries['timeStamp']]
-  
-  switch = {
-    'visible':visPlot,
-    'nir':nirPlot,
-    'swir':swirPlot,
-    'atmospheric':atmPlot
-  }
-  
-  return switch[plotType](timeSeries)
+    # original time series
+    ax.plot(DF[name],color='#1f77b4')
+    ax.set_ylabel(name)
+    ax.set_ylim(ylim)
+    ax.set_xlim(pd.datetime.strptime(startDate,'%Y-%m-%d'),\
+                pd.datetime.strptime(stopDate,'%Y-%m-%d'))
 
+    # boxcar average
+    ax.plot(DF[name].rolling(180).mean(),color='red')
 
+    # make the dates exact
+    ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
 
+def plotTimeSeries(DF, hue_stretch, startDate, stopDate):
 
-
-
+    fig, axH, axS, axV = figure_plotting_space()
+    plot_colorbar(axH,[hue_stretch], ylabel='hue')
+    plot_timeseries(DF, axS,'sat', startDate, stopDate, ylim=[0,1])
+    plot_timeseries(DF, axV,'val', startDate, stopDate)
