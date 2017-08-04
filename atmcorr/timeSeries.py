6e121d502e6ba3b4e2bdf7617d7931b1be66791f
Sam Murphy <samsammurphy@gmail.com>
2017-08-04 13:19:52 -1000

    timeseries saves/loads excel

--- atmcorr/timeSeries.py
+++ atmcorr/timeSeries.py
@@ -1,4 +1,13 @@
-def timeseries_extrator(mission):
+import os
+import ee
+import pandas as pd
+import interpolated_lookup_tables as iLUT
+from ee_requests import request_meanRadiance
+from atmcorr_timeseries import surface_reflectance_timeseries
+from mission_specifics import ee_bandnames
+from mission_specifics import common_bandnames
+
+def timeseries_extrator(geom, startDate, stopDate, mission, removeClouds=True):
     """
     This is the function for extracting atmospherically corrected, 
     cloud-free time series for a given satellite mission.
@@ -29,10 +38,10 @@ def timeseries_extrator(mission):
     
     return timeseries  
 
-def get_allTimeSeries(missions):
+def allTimeSeries(target, geom, startDate, stopDate, missions, removeClouds=True):
     """
     Extracts time series for each mission and join them together
-    """
+    """ 
     
     # will store results here (and use consistent band names)
     allTimeSeries = {
@@ -48,7 +57,7 @@ def get_allTimeSeries(missions):
     # for mission in ['Landsat4']:
     for mission in missions:
         
-        timeseries = timeseries_extrator(mission) 
+        timeseries = timeseries_extrator(geom, startDate, stopDate, mission, removeClouds=removeClouds) 
         
         # names of wavebands
         eeNames = ee_bandnames(mission)
@@ -63,7 +72,7 @@ def get_allTimeSeries(missions):
             if key == 'timeStamp':
                 allTimeSeries['timeStamp'].append(timeseries['timeStamp'])
     
-    # flatten each mission into a single list
+    # flatten each variables (from separate missions) into a single list
     def flatten(multilist):
         if isinstance(multilist[0], list):
             return [item for sublist in multilist for item in sublist]
@@ -72,5 +81,47 @@ def get_allTimeSeries(missions):
 
     for key in allTimeSeries.keys():
         allTimeSeries[key] = flatten(allTimeSeries[key])
-
+    
     return allTimeSeries
+
+def saveToExcel(target, allTimeSeries):
+    basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    excel_dir = os.path.join(basedir,'files','excel')
+    if not os.path.exists(excel_dir):
+        os.makedirs(excel_dir)
+    
+    # create pandas data frame
+    df = pd.DataFrame.from_dict(allTimeSeries)
+
+    # save to excel
+    df.to_excel(os.path.join(excel_dir, target+'.xlsx'), index=False)
+
+def loadFromExcel(target):
+    basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    excel_path = os.path.join(basedir,'files','excel',target+'.xlsx')
+
+    if os.path.isfile(excel_path):
+      print('Loading from excel file')
+      return pd.read_excel(excel_path).to_dict(orient='list')
+
+def timeSeries(target, geom, startDate, stopDate, missions, removeClouds=True):
+    """
+    time series flow
+    1) try loading from excel
+    2) run the extraction
+    3) save to excel
+    """
+
+    # try loading from excel first
+    try:
+      allTimeSeries = loadFromExcel(target)
+      if allTimeSeries:
+        return allTimeSeries
+    except:
+      pass
+       
+    # run extraction
+    allTimeSeries = timeSeries(target, geom, startDate, stopDate, missions)
+
+    # save to excel
+    saveToExcel(target, allTimeSeries)
